c1942228770f60d4290be77567eddd21409a54ea
Sanchez-Diana <sanchez.diana@tecnica7.edu.ar>
2024-12-02 19:49:57 -0300

    aa

---
+++ src/footer.js
@@ -0,0 +1,10 @@
+const footer=`
+<footer id="contenedorfooter" class="footer bg-dark">
+<div class="container text-center">
+    <div class="row text-center text-md-start text-white">
+        7° 1° Programación
+    </div>
+</div>
+</footer>
+`
+export{footer};
